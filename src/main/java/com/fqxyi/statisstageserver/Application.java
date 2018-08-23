e2c0e0607e8c83d9ae52889e74aa8a72356bb7e8
fqxyi <fengqingxiuyi@gmail.com>
2018-08-23 15:44:00 +0800

    部署到服务器

--- src/main/java/com/fqxyi/statisstageserver/Application.java
+++ src/main/java/com/fqxyi/statisstageserver/Application.java
@@ -2,9 +2,34 @@ package com.fqxyi.statisstageserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * 本地
+ */
+
+//@SpringBootApplication
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
+
+/**
+ * 部署
+ */
+
+@ServletComponentScan
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/test/java/com/fqxyi/statisstageserver/ApplicationTests.java
+++ src/test/java/com/fqxyi/statisstageserver/ApplicationTests.java
@@ -3,11 +3,7 @@ package com.fqxyi.statisstageserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,15 +13,15 @@ public class ApplicationTests {
 //    //自动导入依赖的bean。byType方式。把配置好的Bean拿来用，完成属性、方法的组装，它可以对类成员变量、方法及构造函数进行标注，完成自动装配的工作。当加上（required=false）时，就算找不到bean也不报错。
 //    private RedisTemplate<String, ClickNumBean> redisTemplate;
 
-    @Resource
-    private RedisTemplate redisTemplate;
+//    @Resource
+//    private RedisTemplate redisTemplate;
 
     @Test
     public void contextLoads() {
 
-        ValueOperations operations = redisTemplate.opsForValue();
-        operations.set("springboot", "这是一个springboot集成redis的测试数据，字符串操作。");
-        System.out.println(operations.get("springboot"));
+//        ValueOperations operations = redisTemplate.opsForValue();
+//        operations.set("springboot", "这是一个springboot集成redis的测试数据，字符串操作。");
+//        System.out.println(operations.get("springboot"));
 
 //        // 保存对象
 //        ClickNumBean bean = new ClickNumBean();
